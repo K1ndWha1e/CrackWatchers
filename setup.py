976e4d84a9c179cde0633ebe9fbf86b02a4f9292
Nikita <captflint3561@gmail.com>
2019-11-28 00:34:42 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open('README.md', 'r') as readme:
+    description = readme.read()
+
+setup(name='CrackWatchers',
+      version='0.3',
+      description=description,
+      url='https://github.com/K1ndWha1e/CrackWatchers',
+      author='K1ndWha1e',
+      author_email='nikita356@gmail.com',
+      license='Apache-2.0',
+      packages=['CrackWatchers'])
